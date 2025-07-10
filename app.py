d5251ac84b01b3a33cc092eeae0111578bd7583a
KR3915 <ondrej.brdek@gmail.com>
2025-07-10 20:17:26 +0200

    save

--- app.py
+++ app.py
@@ -3,19 +3,19 @@ import subprocess
 import os
 import logging
 from datetime import datetime
+from flask_socketio import SocketIO
+from ptyprocess import PtyProcessUnicode
 
 app = Flask(__name__)
+socketio = SocketIO(app, async_mode="threading", logger=True, engineio_logger=True)
 
-# Cesta k logu
+pty_proc = None
+
+# Logování
 log_path = "logs/log.txt"
 ip_log_path = "logs/user_logs.txt"
-# Vytvoření složky logs, pokud neexistuje
 os.makedirs(os.path.dirname(log_path), exist_ok=True)
 os.makedirs(os.path.dirname(ip_log_path), exist_ok=True)
-
-#nastavení času
-time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-# Nastavení logování
 
 logging.basicConfig(
     filename=log_path,
@@ -27,8 +27,9 @@ logging.basicConfig(
 @app.route("/", methods=["GET", "POST"])
 def index():
     user_ip = request.remote_addr
+    time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     logging.info(f"Request from {user_ip}")
-    with open(ip_log_path, "a", encoding="utf-8") as f: #log ip
+    with open(ip_log_path, "a", encoding="utf-8") as f:
         f.write(f"\n[{time}] {user_ip}")
     
     if request.method == "POST":
@@ -39,5 +40,29 @@ def index():
 
     return render_template("index.html")
 
+@socketio.on('connect')
+def on_connect():
+    global pty_proc
+    if pty_proc is None:
+        pty_proc = PtyProcessUnicode.spawn(['bash'])
+        socketio.start_background_task(read_output)
+
+def read_output():
+    while True:
+        try:
+            if pty_proc is not None:
+                output = pty_proc.read(1024)
+                if output:
+                    print(f"[DEBUG] OUTPUT: {output}")
+                    socketio.emit('output', output)
+        except Exception as e:
+            socketio.sleep(0.1)
+
+
+@socketio.on('input')
+def on_input(data):
+    if pty_proc:
+        pty_proc.write(data)
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    socketio.run(app, host="0.0.0.0", port=5000)
